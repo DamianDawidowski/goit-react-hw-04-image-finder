66c000cf574f721af215cf5726d75d1d9bcfd66f
Damian Dawidowski <damiandawidowski@gmail.com>
2023-06-07 19:42:47 +0200

    modified:   src/components/Error/Error.jsx

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,15 +1,20 @@
-// import css from './Error.module.css';
-// import errImage from './error.png';
+import css from './Error.module.css';
+import errImage from './error.png';
+import PropTypes from 'prop-types';
 
-// const Error = ({ errorText }) => {
-//   return (
-//     <div role="alert" className={css.wrapper}>
-//       <img src={errImage} width="252" alt="error" />
-//       <p text={errorText} className={css.text}>
-//         {errorText}
-//       </p>
-//     </div>
-//   );
-// };
+const Error = ({ errorText }) => {
+  return (
+    <div role="alert" className={css.wrapper}>
+      <img src={errImage} width="252" alt="error" />
+      <p text={errorText} className={css.text}>
+        {errorText}
+      </p>
+    </div>
+  );
+};
 
-// export default Error;
+export default Error;
+
+Error.propTypes = {
+  onSubmit: PropTypes.string,
+};
