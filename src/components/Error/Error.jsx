fe60ed39d9476ad2e6136367f420965abb3c171e
Damian Dawidowski <damiandawidowski@gmail.com>
2023-06-07 19:33:12 +0200

    modified:   src/components/Error/Error.jsx

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,15 +1,15 @@
-import css from './Error.module.css';
-import errImage from './error.png';
+// import css from './Error.module.css';
+// import errImage from './error.png';
 
-const Error = ({ errorText }) => {
-  return (
-    <div role="alert" className={css.wrapper}>
-      <img src={errImage} width="252" alt="error" />
-      <p text={errorText} className={css.text}>
-        {errorText}
-      </p>
-    </div>
-  );
-};
+// const Error = ({ errorText }) => {
+//   return (
+//     <div role="alert" className={css.wrapper}>
+//       <img src={errImage} width="252" alt="error" />
+//       <p text={errorText} className={css.text}>
+//         {errorText}
+//       </p>
+//     </div>
+//   );
+// };
 
-export default Error;
+// export default Error;
