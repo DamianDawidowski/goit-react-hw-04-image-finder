f15faa7a7f2079a4f47ee6f6f6958e453a01c573
Damian Dawidowski <damiandawidowski@gmail.com>
2023-06-07 19:13:53 +0200

    modified:   src/components/Error/Error.jsx

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -4,7 +4,7 @@ import errImage from './error.png';
 const Error = ({ errorText }) => {
   return (
     <div role="alert" className={css.wrapper}>
-      <img src={errImage} width="250" alt="error" />
+      <img src={errImage} width="251" alt="error" />
       <p text={errorText} className={css.text}>
         {errorText}
       </p>
