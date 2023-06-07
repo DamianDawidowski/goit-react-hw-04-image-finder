597c271d0723995ab606cb3c31b1b4c998f6766b
Damian Dawidowski <damiandawidowski@gmail.com>
2023-06-07 20:43:08 +0200

    modified:   src/components/ImageGalleryItem/ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,21 +1,10 @@
-import Modal from 'components/Modal/Modal';
 import css from './ImageGalleryItem.module.css';
 import propTypes from 'prop-types';
 import { Component } from 'react';
 
 class ImageGalleryItem extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     isShown: false,
-  //   };
-  // }
-  // handleClick = ev => {
-  //   this.setState({ isShown: !this.state.isShown });
-  // };
-
   render() {
-    // let isShown = this.state.isShown;
+    
     const { webformatURL, largeImageURL, editModal } = this.props;
     return (
       <li className={css.ImageGalleryItem + ' ' + css.ImageGalleryItemImage}>
